aaa6487e1e51991b193d6ef4777dd603b0b9e058
Usurer <ivvlg@narod.ru>
2017-09-12 12:37:01 +0300

    Adding Web project

---
+++ WebClient/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Hosting;
+using System.Web.Mvc;
+using Autofac;
+using GDriveClientLib.Abstractions;
+using LocalFileSystemLib;
+
+namespace WebClient.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Worker/Initializer.cs
@@ -0,0 +1,50 @@
+﻿using System.IO;
+using System.Threading;
+using Autofac;
+using GDriveClientLib.Abstractions;
+using GDriveClientLib.Implementations;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v2;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+
+namespace Worker
+{
+    public class Initializer
+    {
+        public IContainer RegisterComponents(string appDataPath)
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<Uploader>().SingleInstance().As<IUploader>();
+            builder.RegisterType<Reader>().SingleInstance().As<IReader>();
+
+            var service = InitializeDriveService(appDataPath);
+            builder.RegisterInstance(service).SingleInstance().As<IGoogleDriveService>();
+
+            return builder.Build();
+        }
+
+        private IGoogleDriveService InitializeDriveService(string appDataPath)
+        {
+            UserCredential credential;
+            
+            using (var filestream = new FileStream($"{appDataPath}\\client_secrets.json", FileMode.Open, FileAccess.Read))
+            {
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(filestream).Secrets,
+                    new[] { DriveService.Scope.Drive },
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(appDataPath, true))
+                    .Result;
+            }
+
+            return new GoogleDriveService(new BaseClientService.Initializer
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = "Drive API Sample",
+            });
+        }
+    }
+}
