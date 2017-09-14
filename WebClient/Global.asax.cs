51f0a77b209633caa6214d272a2532e285d2c756
Usurer <ivvlg@narod.ru>
2017-09-14 19:09:43 +0300

    log4net added

--- WebClient/Global.asax.cs
+++ WebClient/Global.asax.cs
@@ -19,6 +19,8 @@ namespace WebClient
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            log4net.Config.XmlConfigurator.Configure();
         }
     }
 }
